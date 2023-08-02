36669770d3c2a95dd854014e85a76d55d54eb196
bomanviktor <boman.viktor@gmail.com>
2023-08-02 15:48:00 +0300

    Remove the cookie invalidation

--- js/components/sidebar.js
+++ js/components/sidebar.js
@@ -2,7 +2,6 @@ import {Auth} from "./auth.js"
 
 export const sidebarController = () => {
   document.getElementById("logout").addEventListener("click", () => {
-    document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
     Auth(false)
   })
 }
